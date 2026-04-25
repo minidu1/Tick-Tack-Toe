685bb3634e87b08ced6c3fcab8fbd61bc4064b68
minidu1 <lmmdsilva@students.nsbm.ac.lk>
2026-04-25 13:35:24 +0530

    make new arrays inside game board instead of copying same array

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function GameBoard(){
   const row = 3
   const column = 3
 
+  // use for loop and put array into places i want instead of puahing
   for (let i=0; i<row; i++){
-    board[i] = cell
+    board[i] = []
     for (let j=0; j<column; j++){
-      cell[j]= null
+      board[i][j]= null
     }
   }
   console.log(board)
